d490e9899e6f7669462f17c7509fbaa305dd64f2
yura.vataschuk <yura.vataschuk@gmail.com>
2023-05-22 21:56:02 +0200

    Changed book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -8,6 +9,15 @@ class Book(models.Model):
 
     title = models.CharField(unique=True, max_length=255)
     author = models.CharField(max_length=255)
-    Cover = models.CharField(max_length=255, choices=CoverChoices.choices)
-    inventory = models.IntegerField()
+    Cover = models.CharField(
+        max_length=4,
+        choices=CoverChoices.choices
+    )
+    inventory = models.IntegerField(validators=(MinValueValidator(1),))
     daily_fee = models.DecimalField(max_digits=55, decimal_places=2)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return f"{self.title} (authors: {self.author})"
--- books/views.py
+++ books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books.models import Book
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
